37a66e81a0887a4ec11a742217b468466580ce89
sugus11 <96228424+sugus11@users.noreply.github.com>
2021-12-16 09:10:57 +0100

    Add files via upload

--- notendurchschnitt.py
+++ notendurchschnitt.py
@@ -1,6 +1,8 @@
-note=[4.5, 5.5]  #
+# Noteneingabe
+note=[4.5, 5.5]
 anzahl=len(note)
 summe=0
-for n in note:
+for n in note: 
     summe=summe+n
-print("Notendurchschnitt" + str(summe/anzahl))
+# Ausgabe von Notendurchschnitt
+print("Notendurchschnitt" + str(summe/anzahl)) 
